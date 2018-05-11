d2221197b0f69f116700d9594d1716144dd4ddaa
Deepak Negi <deepak.negi41@gmail.com>
2018-05-11 21:54:28 +0530

    changes made

--- app/src/main/java/com/example/dell/courtcounter/FinalScores.java
+++ app/src/main/java/com/example/dell/courtcounter/FinalScores.java
@@ -9,6 +9,7 @@ import org.w3c.dom.Text;
 public class FinalScores extends AppCompatActivity {
     TextView teama;
     TextView teamb;
+    TextView winner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +17,18 @@ public class FinalScores extends AppCompatActivity {
         setContentView(R.layout.activity_final_scores);
         teama=(TextView)findViewById(R.id.textView4);
         teamb=(TextView)findViewById(R.id.textView7);
+        winner=(TextView)findViewById(R.id.textView5);
         Bundle b=getIntent().getExtras();
         teama.setText(String.valueOf(b.getInt("team_a")));
         teamb.setText(String.valueOf(b.getInt("team_b")));
+        if((b.getInt("team_a")>b.getInt("team_b")))
+        {
+            winner.setText("TEAM A is the Winner!!!");
+        }
+        else
+        {
+            winner.setText("TEAM B is the Winner!!!");
+        }
     }
 
 }
